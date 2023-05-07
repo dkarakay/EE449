298c41b2f3deb572ce1ed254090d536bfa2f9c1c
Deniz Karakay <deniz@karakay.me>
2023-05-08 01:56:00 +0300

    Update HW2

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -1,6 +1,9 @@
 # Created by Deniz Karakay on 30.04.2023
 # Description: This file contains the main program for the second homework of EE449 course.
+import copy
+import os
 import random
+import shutil
 import time
 import cv2
 import numpy as np
@@ -8,7 +11,6 @@ import numpy as np
 IMG_PATH = "painting.png"
 IMG_WIDTH = 0
 IMG_HEIGHT = 0
-MUTATION_PROB = 0.2
 
 
 class Gene:
@@ -27,10 +29,16 @@ class Gene:
                 x = self.x + random.randint(-IMG_WIDTH // 4, IMG_WIDTH // 4)
                 y = self.y + random.randint(-IMG_HEIGHT // 4, IMG_HEIGHT // 4)
             else:
-                x = random.randint(-self.radius, IMG_WIDTH + self.radius)
-                y = random.randint(-self.radius, IMG_HEIGHT + self.radius)
+                x = random.randint(-self.radius * 2, IMG_WIDTH + self.radius * 2)
+                y = random.randint(-self.radius * 2, IMG_HEIGHT + self.radius * 2)
 
-            if (x + self.radius) < IMG_WIDTH and (y + self.radius) < IMG_HEIGHT:
+            # Check if some part of the circle is inside the image although center point is outside
+            if (
+                x - self.radius >= 0
+                and x + self.radius <= IMG_WIDTH
+                and y - self.radius >= 0
+                and y + self.radius <= IMG_HEIGHT
+            ):
                 return x, y
 
     # Mutate the gene with a guided mutation
@@ -68,17 +76,18 @@ class Gene:
 
 
 class Individual:
-    def __init__(self, num_genes, id=-2, fitness=0):
+    def __init__(self, num_genes, id=-2, fitness=0, chromosome=[]):
         self.id = id
-        self.chromosome = []
+        self.chromosome = chromosome
         self.fitness = fitness
 
-        # Create a chromosome with num_genes
-        for i in range(num_genes):
-            self.chromosome.append(Gene(i))
+        if len(chromosome) == 0:
+            # Create a chromosome with num_genes
+            for i in range(num_genes):
+                self.chromosome.append(Gene(i))
 
-        # Sort chromosomes by radius by descending order
-        self.chromosome.sort(key=lambda x: x.radius, reverse=True)
+            # Sort chromosomes by radius by descending order
+            self.chromosome.sort(key=lambda x: x.radius, reverse=True)
 
     # Draw the individual to the screen
     def draw(self):
@@ -119,10 +128,18 @@ class Individual:
 
         return self.fitness
 
-    def mutate(self, mutation_type):
-        while random.random() < MUTATION_PROB:
+    def mutate(self, mutation_type, mutation_prob):
+        mutations = []
+        while random.random() < mutation_prob:
             random_gene_id = random.randint(0, len(self.chromosome) - 1)
 
+            if len(mutations) >= len(self.chromosome):
+                return
+
+            while random_gene_id in mutations:
+                random_gene_id = random.randint(0, len(self.chromosome) - 1)
+
+            mutations.append(random_gene_id)
             if mutation_type == "unguided":
                 # print("Unguided Mutation")
                 self.chromosome[random.randint(0, len(self.chromosome) - 1)] = Gene(
@@ -147,13 +164,14 @@ class Individual:
 
 
 class Population:
-    def __init__(self, num_inds, num_genes):
-        self.population = []
+    def __init__(self, num_inds, num_genes, population=[]):
+        self.population = population
         self.best_population = []
 
-        # Create a population with num_inds individuals
-        for i in range(num_inds):
-            self.population.append(Individual(id=i, num_genes=num_genes))
+        if len(population) == 0:
+            # Create a population with num_inds individuals
+            for i in range(num_inds):
+                self.population.append(Individual(id=i, num_genes=num_genes))
 
     # Evaluate every individual in the population
     def evaluate(self):
@@ -165,36 +183,70 @@ class Population:
         t = sorted(self.population, key=lambda x: x.fitness, reverse=True)
         return t
 
+    # Sort the population by fitness in descending order
+    def sort_inds(self, pop):
+        return sorted(pop, key=lambda x: x.fitness, reverse=True)
+
     # Select the best num_elites individuals in the population
     # Return the best num_elites individuals and the rest of the population
-    def selection(self, num_elites):
+    def selection(self, fraction_elites, fraction_parents, tm_size):
         self.population = self.sort_population()
-        return self.population[:num_elites], self.population[num_elites:]
 
-    # Select the best individual in the population using tournament selection
-    def tournament_selection(self, non_elites, tm_size):
-        tournament = random.sample(non_elites, tm_size)
-        return max(tournament, key=lambda x: x.fitness)
+        # Select the best num_elites individuals in the population
+        num_elites = int(len(self.population) * fraction_elites)
+
+        # Select the best num_parents individuals in the population
+        num_parents = int(len(self.population) * fraction_parents)
+
+        # If the number of parents is odd, make it even
+        if num_parents % 2 == 1:
+            num_parents += 1
+
+        # Divide the population into elites and non_elites
+        elites = self.population[:num_elites]
+        non_elites = self.population[num_elites:]
+
+        # Select num_parents individuals from the non_elites using tournament selection
+        parents = []
+        for i in range(num_parents):
+            tournament = random.sample(non_elites, tm_size)
+            best = self.sort_inds(tournament)[0]
+            parents.append(best)
+            non_elites.remove(best)
+
+        return elites, non_elites, parents
 
     # Select two parents from the population using tournament selection
-    def crossover(self, parents):
-        parent1, parent2 = parents
+    def crossover(self, parents, num_genes):
+        children = []
+        for i in range(0, len(parents), 2):
+            parent1 = parents[i]
+            parent2 = parents[i + 1]
 
-        # Create two children with the same length as parents
-        child1 = Individual(len(parent1.chromosome))
-        child2 = Individual(len(parent2.chromosome))
+            chromosome_child1 = []
+            chromosome_child2 = []
 
-        for i in range(len(parent1.chromosome)):
-            # 50% chance to get the gene from parent1 or parent2
-            if random.random() >= 0.5:
-                child1.chromosome[i] = parent1.chromosome[i]
-                child2.chromosome[i] = parent2.chromosome[i]
+            for j in range(len(parent1.chromosome)):
+                # 50% chance to get the gene from parent1 or parent2
+                if random.random() >= 0.5:
+                    chromosome_child1.append(copy.deepcopy(parent1.chromosome[j]))
+                    chromosome_child2.append(copy.deepcopy(parent2.chromosome[j]))
+                # 50% chance to get the gene from parent2 or parent1
+                else:
+                    chromosome_child1.append(copy.deepcopy(parent2.chromosome[j]))
+                    chromosome_child2.append(copy.deepcopy(parent1.chromosome[j]))
 
-            # 50% chance to get the gene from parent2 or parent1
-            else:
-                child1.chromosome[i] = parent2.chromosome[i]
-                child2.chromosome[i] = parent1.chromosome[i]
-        return child1, child2
+            #   Create two children with the same length as parents
+            child1 = Individual(num_genes=num_genes, chromosome=chromosome_child1)
+            child2 = Individual(num_genes=num_genes, chromosome=chromosome_child2)
+            child1.eval()
+            child2.eval()
+
+            pop = Population.sort_inds(self, [child1, child2, parent1, parent2])
+            children.append(pop[0])
+            children.append(pop[1])
+
+        return children
 
     # Print every property of the population in one line
     def print(self):
@@ -202,71 +254,85 @@ class Population:
             individual.print()
 
 
+def save_population(population, path):
+    import pickle
+
+    with open(path + ".pkl", "wb") as f:
+        pickle.dump(population, f)
+
+
 def evaluationary_algorithm(
-    num_inds,
-    num_genes,
-    num_generations,
-    num_elites=5,
+    name,
+    num_generations=10000,
+    num_inds=20,
+    num_genes=50,
     tm_size=5,
-    num_parents=4,
     mutation_type="guided",
+    fraction_elites=0.2,
+    fraction_parents=0.6,
+    mutation_prob=0.2,
 ):
+    path = f"results/{name}/"
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+
     pop = Population(num_inds, num_genes)
 
-    # population.print()
     for generation in range(num_generations):
         pop.evaluate()
 
         # Select the best num_elites individuals in the population and the rest of the population
-        elites, non_elites = pop.selection(num_elites)
-
-        parents = []
-        for i in range(num_parents):
-            parents.append(pop.tournament_selection(non_elites, tm_size))
+        elites, non_elites, parents = pop.selection(
+            fraction_elites, fraction_parents, tm_size
+        )
 
         # Create children from the parents
-        children = []
-        for i in range(0, len(parents), 2):
-            child1, child2 = pop.crossover(parents[i : i + 2])
-            children.append(child1)
-            children.append(child2)
+        children = pop.crossover(parents, num_genes)
 
+        # Mutate the children and non_elites
         for individual in non_elites + children:
-            individual.mutate(mutation_type)
+            individual.mutate(mutation_type, mutation_prob)
 
         # Add the elites, children and non_elites to the population
         pop.population = elites + children + non_elites
-        sorted_population = pop.sort_population()
-        best = max(pop.population, key=lambda x: x.fitness)
+
+        if generation % 1000 == 0 and generation != 0:
+            sorted_population = pop.sort_population()
+            print(sorted_population[0].fitness)
+            pop.best_population.append(sorted_population[0])
+
+            current_name = f"{name}_{generation}"
+            file_path = f"{path}{current_name}"
+            save_population(pop, file_path)
+
+            cv2.imwrite(f"{path}{current_name}.png", sorted_population[0].draw())
 
         if generation % 100 == 0:
-            sorted_population = pop.sort_population()
-            pop.best_population.append(sorted_population[0])
-            # for p in pop.best_population:
-            #    name = self.name + "_" + str(p.id) + ".png"
-            cv2.imwrite(f"results/{generation}.png", pop.best_population[0].draw())
+            print("Generation:", generation)
 
-    best = max(pop.population, key=lambda x: x.fitness)
-    cv2.imshow("image", best.draw())
-    cv2.waitKey(0)
+    pop.evaluate()
+    sorted_population = pop.sort_population()
+    pop.best_population.append(sorted_population[0])
+    current_name = name + "_" + str(generation + 1)
+    file_path = f"{path}{current_name}"
+    save_population(pop, file_path)
+    cv2.imwrite(f"{path}{current_name}.png", pop.best_population[0].draw())
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    num_inds = 20
-    num_genes = 50
 
     img = cv2.imread(IMG_PATH)
     IMG_WIDTH = img.shape[0]
     IMG_HEIGHT = img.shape[1]
 
     IMG_RADIUS = (IMG_WIDTH + IMG_HEIGHT) / 2
-    print(IMG_WIDTH, IMG_HEIGHT)
+    # print(IMG_WIDTH, IMG_HEIGHT)
 
-    evaluationary_algorithm(
-        num_inds=num_inds,
-        num_genes=num_genes,
-        num_generations=1000,
-    )
+    evaluationary_algorithm(name="default")
 
     print("Execution time:", time.time() - start_time)
